db02c0109d02060b4c3fcd3f0888c630cc0586e5
Anshu <homeless.pvt@gmail.com>
2024-11-08 13:11:00 +0530

    Initial commit from Create Next App

---
+++ app/page.js
@@ -0,0 +1,17 @@
+import Image from "next/image";
+import ".page.css"
+
+
+export default function Home() {
+  return (
+<html> 
+
+    <head>
+
+    </head>
+    <main>
+
+    </main>
+</html>
+  );
+}
